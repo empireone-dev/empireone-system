ebe4db0b0e7aeb1b5239ad899ddc0dcbc22baf54
QuicklyDG <quicklydeguzman@gmail.com>
2025-09-29 23:11:04 +0800

    added legal department

--- resources/js/app/pages/administrator/ticketing/_sections/create-ticket-section.jsx
+++ resources/js/app/pages/administrator/ticketing/_sections/create-ticket-section.jsx
@@ -58,7 +58,7 @@ export default function CreateTicketSection() {
 
         // Append normal fields
         Object.entries(data).forEach(([key, value]) => {
-            if (key === "date_range") return; 
+            if (key === "date_range") return;
             if (key === "files" && value?.length) {
                 Array.from(value).forEach((file, index) => {
                     console.log("filefile", file);
